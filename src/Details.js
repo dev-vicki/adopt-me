1f3908ad61bdd62d19376d33d16696032a88f3be
dev-vicki <vignesh.r1999@gmail.com>
2021-10-27 17:59:32 +0530

    implemented carousel -A array of images of same animal.

--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,9 @@
 import { Component } from 'react'
 import { withRouter } from 'react-router-dom'
-
+import Carousel from './Carousel';
 class Details extends Component {
 
-    state = { loading: true};
+      state = { loading: true};
 
     async componentDidMount(){
       const res = await fetch(
@@ -20,9 +20,10 @@ class Details extends Component {
     }
 
 
-    const {name, animal, breed, city, state, description} = this.state;
+    const {name, animal, breed, city, state, description, images} = this.state;
   return(
     <div className="details">
+    <Carousel images={images} />
       <div>
         <h1>{name}</h1>
         <h2>{animal} - {breed} - {city}, {state}</h2>
