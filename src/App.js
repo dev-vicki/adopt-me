7d715f09e3591ad4493010081d05c481aa9213ee
dev-vicki <vignesh.r1999@gmail.com>
2021-10-27 16:56:52 +0530

    implemented react-router with two switches

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
-      <h1 id="my-brand">Adopt Me!</h1>
+    <Router>
+
+    <header>
+    <Link to="/">
+      <h1>Adopt Me!</h1>
+    </Link>
+    </header>
+
+      <Switch>
+        <Route path="/details/:id">
+          <Details />
+        </Route>
+
+        <Route path="/">
       <SearchParams />
+        </Route>
+      </Switch>
+      </Router>
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
