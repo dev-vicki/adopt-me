ac23e5ca88bfd034d61c812252ad1f5609435986
dev-vicki <vignesh.r1999@gmail.com>
2021-10-27 18:09:23 +0530

    added interactivity to images.

--- src/Carousel.js
+++ src/Carousel.js
@@ -7,7 +7,14 @@ class Carousel extends Component{
 
     static defaultProps = {
         images: ['http://pets-images.dev-apis.com/pets/none.jpg']
+    };
+
+    handleIndexClick = (event) => {
+        this.setState({
+            active: +event.target.dataset.index,
+        });
     }
+
     render() {
         const { active} = this.state;
         const { images} = this.props;
@@ -16,9 +23,12 @@ class Carousel extends Component{
             <div className="carousel">
                 <img src={images[active]} alt="animal" />
                 <div className="carousel-smaller">{images.map((photo, index) =>(
+                    // eslint-disable-next-line
                     <img
                         key={photo}
                         src={photo}
+                        data-index={index}
+                        onClick={this.handleIndexClick.bind(this)}
                         className={index === active ? "active" : ""}
                         alt="animal thumbnail"
                     />
